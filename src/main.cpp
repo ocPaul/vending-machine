d10981f580adf2d9b14f24dbe97eb87494df92de
Simon Schafferhofer <schaff190163@sr.htlweiz.at>
2023-01-24 12:07:46 +0100

    Buttons Initialized and Current state read

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
-}
 
+
+  // initializes Start Button as Pull Up Input
+  pinMode(14, INPUT_PULLUP);
+  pinMode(18, INPUT_PULLDOWN);
+  pinMode(19, INPUT_PULLDOWN);
+  pinMode(23, INPUT_PULLDOWN);
+}
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //reads state of Start Button
+  int startButtonCurrState = digitalRead(14);
+  int tipSwich01CurrState = digitalRead(18);
+  int tipSwich02CurrState = digitalRead(19);
+  int tipSwich03CurrState = digitalRead(23);
 }

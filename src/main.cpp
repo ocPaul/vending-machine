d276ae5c87d69a9a5cb6c5ad6519c96c49f0fc41
ocPaul <stranz190097@sr.htlweiz.at>
2023-01-24 12:09:19 +0100

    servo functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "main.h"
 
+int servoPin_1 = 13;
+int servoPin_2 = 12;
+int freq = 50;
+int channel_1 = 0;
+int channel_2 = 1;
+int resolution = 16;
+
 
 VENDING_STATE poll_input() {
 
@@ -13,12 +20,16 @@ VENDING_STATE motor_backward() {
 
 }
 
-VENDING_STATE servo_on() {
-
+VENDING_STATE servo_1() {
+    ledcWrite(channel_1, 3277);
+    delay(1000);
+    ledcWrite(channel_1, 6553);
 }
 
-VENDING_STATE servo_off() {
-  
+VENDING_STATE servo_2() {
+    ledcWrite(channel_2, 3277);
+    delay(1000);
+    ledcWrite(channel_2, 6553);
 }
 
 void state_machine_poll() {
@@ -34,6 +45,12 @@ void state_machine_poll() {
 StepperController stepper_controller = StepperController(COIL_1, COIL_2, COIL_3, COIL_4);
 
 void setup() {
+  ledcSetup(channel_1, freq, resolution);
+  ledcSetup(channel_2, freq, resolution);
+
+  ledcAttachPin(servoPin_1, channel_1);
+  ledcAttachPin(servoPin_2, channel_2);
+  
   stepper_controller.MoveSteps(5, COUNTERCLOCKWISE);
 }
 

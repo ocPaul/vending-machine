da8642cf4734fa84df78fe8438a1f98ffb86a452
MichaelGloessl04 <55692241+MichaelGloessl04@users.noreply.github.com>
2023-01-24 11:08:20 +0100

    implemeted stepper_controller package from old assignment

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <stepper_controller.h>
+
+#define COIL_1 33
+#define COIL_2 32
+#define COIL_3 35
+#define COIL_4 34
+
+StepperController stepper_controller = StepperController(COIL_1, COIL_2, COIL_3, COIL_4);
 
 void setup() {
-  // put your setup code here, to run once:
+  stepper_controller.MoveSteps(5, COUNTERCLOCKWISE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delayMicroseconds(1000);
+	stepper_controller.Step(COUNTERCLOCKWISE);
 }
